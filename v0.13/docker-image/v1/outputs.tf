7e9f5f21d6ff53d5da0d1f5a9d8b8db070cd4844
erik-ekberg-sg <erik@singlegrain.com>
2020-09-30 14:17:44 -0700

    Use cname instead of load balancer URL

--- v0.13/docker-image/v1/outputs.tf
+++ v0.13/docker-image/v1/outputs.tf
@@ -1,6 +1,3 @@
-output "cname" {
-	value = aws_elastic_beanstalk_environment.default.cname
-}
 output "url" {
-	value = aws_elastic_beanstalk_environment.default.endpoint_url
+	value = aws_elastic_beanstalk_environment.default.cname
 }
