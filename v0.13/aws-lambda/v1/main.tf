63bf477ee5bfc23d1e8ec544acf4044d49db6b14
erik-ekberg-sg <erik@singlegrain.com>
2020-10-05 18:42:49 -0700

    Create aws-lambda/v1 terraform module

---
+++ v0.13/aws-lambda/v1/main.tf
@@ -0,0 +1,42 @@
+locals {
+	lambda_zip = "terraform_lambda.zip"
+}
+
+data "archive_file" "default" {
+	excludes = concat([local.lambda_zip], var.excluded_files)
+	output_path = "${var.source_directory}/${local.lambda_zip}"
+	source_dir = var.source_directory
+	type = "zip"
+}
+data "aws_iam_policy_document" "default" {
+	version = "2012-10-17"
+
+	statement {
+		actions = ["sts:AssumeRole"]
+		effect = "Allow"
+
+		principals {
+			identifiers = ["lambda.amazonaws.com"]
+			type = "Service"
+		}
+	}
+}
+resource "aws_iam_role" "default" {
+	assume_role_policy = data.aws_iam_policy_document.default.json
+}
+resource "aws_lambda_function" "default" {
+	filename = local.lambda_zip
+	function_name = substr("${var.name}--${random_id.default.hex}", 0, 64)
+	handler = var.handler
+	role = aws_iam_role.default.arn
+	runtime = var.runtime
+	source_code_hash = data.archive_file.default.output_base64sha256
+
+	environment {
+		// * Environmental keys must not container hyphens "-" https://stackoverflow.com/a/60885479
+		variables = merge([for env in var.environment: { (env["key"]) = (env["value"]) }]...)
+	}
+}
+resource "random_id" "default" {
+	byte_length = 32
+}
---
+++ v0.13/aws-lambda/v1/providers.tf
@@ -0,0 +1,18 @@
+terraform {
+	required_version = "~> 0.13.0"
+
+	required_providers {
+		archive = {
+			source = "hashicorp/archive"
+			version = "~> 1.3.0"
+		}
+		aws = {
+			source = "hashicorp/aws"
+			version = "~> 3.0"
+		}
+		random = {
+			source = "hashicorp/random"
+			version = "~> 2.3.0"
+		}
+	}
+}
