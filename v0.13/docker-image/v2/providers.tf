7c9dd3f50aa3c33254270987936c26e18bbcc47d
erik-ekberg-sg <erik@singlegrain.com>
2020-11-19 13:56:23 -0800

    Update support for providers

---
+++ v0.13/docker-image/v2/providers.tf
@@ -0,0 +1,18 @@
+terraform {
+	required_version = "~> 0.13.0"
+
+	required_providers {
+		aws = {
+			source = "hashicorp/aws"
+			version = "~> 3.0.0"
+		}
+		random = {
+			source = "hashicorp/random"
+			version = "~> 3.0.0"
+		}
+		time = {
+			source = "hashicorp/time"
+			version = "~> 0.6.0"
+		}
+	}
+}
