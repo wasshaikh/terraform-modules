a30fb730c9a8a5c00dc7d937a4eee29dbf5044b7
erik-ekberg-sg <erik@singlegrain.com>
2020-11-19 11:41:37 -0800

    Revert to old provider defaults

--- v0.13/aws-lambda/v1/providers.tf
+++ v0.13/aws-lambda/v1/providers.tf
@@ -4,15 +4,15 @@ terraform {
 	required_providers {
 		archive = {
 			source = "hashicorp/archive"
-			version = "~> 2.0.0"
+			version = "~> 1.3.0"
 		}
 		aws = {
 			source = "hashicorp/aws"
-			version = "~> 3.0.0"
+			version = "~> 3.0"
 		}
 		random = {
 			source = "hashicorp/random"
-			version = "~> 3.0.0"
+			version = "~> 2.3.0"
 		}
 	}
 }
