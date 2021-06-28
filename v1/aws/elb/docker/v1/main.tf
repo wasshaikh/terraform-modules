be2e28fc1bb68ba9c1dd97ab589fb97a9c1eb4c3
erik-ekberg-sg <erik@singlegrain.com>
2021-06-28 14:48:59 -0700

    Create terraform v1 modules

---
+++ v1/aws/elb/docker/v1/main.tf
@@ -0,0 +1,140 @@
+// Create IAM role to manage elasticbeanstalk
+resource "aws_iam_role_policy_attachment" "default" {
+	// ? Is this appropriate access ?
+	policy_arn  = "arn:aws:iam::aws:policy/AdministratorAccess"
+	role = aws_iam_role.default.name
+}
+resource "aws_iam_role" "default" {
+	assume_role_policy = data.aws_iam_policy_document.default.json
+}
+data "aws_iam_policy_document" "default" {
+	version = "2012-10-17"
+
+	statement {
+		actions = ["sts:AssumeRole"]
+		effect = "Allow"
+
+		principals {
+			identifiers = ["ec2.amazonaws.com"]
+			type = "Service"
+		}
+	}
+}
+resource "aws_iam_instance_profile" "default" {
+	role = aws_iam_role.default.name
+}
+
+// Create environment
+resource "random_id" "default" {
+	byte_length = 40
+}
+resource "aws_elastic_beanstalk_application" "default" {
+	name = random_id.default.b64_url
+}
+
+// Create release
+resource "aws_s3_bucket" "default" {
+}
+resource "aws_s3_bucket_object" "authentication" {
+	bucket = aws_s3_bucket.default.id
+	key = "docker-config.json"
+	content = jsonencode({
+		auths = {
+			(split("/", var.image)[0]) = {
+				"password" = var.registry_password
+				"username" = var.registry_username
+			}
+		}
+	})
+}
+resource "aws_s3_bucket_object" "default" {
+	bucket = aws_s3_bucket.default.id
+	key = "Dockerrun.aws.json"
+	content = jsonencode({
+		"AWSEBDockerrunVersion" = "1"
+		"Authentication" = {
+			"Bucket" = aws_s3_bucket.default.id
+			"Key" = aws_s3_bucket_object.authentication.id
+		}
+		"Image" = {
+			"Name" = var.image
+			"Update" = "true"
+		}
+		"Ports" = [
+			{ "ContainerPort" = var.port }
+		]
+	})
+	content_type = "application/json"
+}
+resource "time_sleep" "wait_for_s3_object" {
+	create_duration = "3s"
+	depends_on = [
+		aws_s3_bucket_object.authentication,
+		aws_s3_bucket_object.default,
+	]
+}
+resource "aws_elastic_beanstalk_application_version" "default" {
+	application = aws_elastic_beanstalk_application.default.name
+	bucket = aws_s3_bucket.default.id
+	key = "Dockerrun.aws.json"
+	// For display purposes, get last 100 characters for version label
+	name = strrev(substr(strrev(replace(var.image, "/", "\\")), 0, 100))
+
+	depends_on = [time_sleep.wait_for_s3_object]
+}
+
+// Create Application
+resource "aws_elastic_beanstalk_environment" "default" {
+	application = aws_elastic_beanstalk_application.default.name
+	name = var.name
+	solution_stack_name = var.solution_stack_name
+	tier = var.type == "website" ? "WebServer" : "Worker"
+	version_label = aws_elastic_beanstalk_application_version.default.id
+
+	setting {
+		name = "DeploymentPolicy"
+		namespace = "aws:elasticbeanstalk:command"
+		value = "RollingWithAdditionalBatch"
+	}
+	setting {
+		name = "IamInstanceProfile"
+		namespace = "aws:autoscaling:launchconfiguration"
+		value = aws_iam_instance_profile.default.name
+	}
+	setting {
+		name = "InstanceTypes"
+		namespace = "aws:ec2:instances"
+		value = "t3.small"
+	}
+	setting {
+		name = "MaxSize"
+		namespace = "aws:autoscaling:asg"
+		value = var.max
+	}
+	setting {
+		name = "MinSize"
+		namespace = "aws:autoscaling:asg"
+		value = var.min
+	}
+
+	// Environmental settings
+	dynamic "setting" {
+		for_each = var.environment
+		content {
+			name = setting.value["key"]
+			namespace = "aws:elasticbeanstalk:application:environment"
+			value = setting.value["value"]
+		}
+	}
+
+	// Advanced settings
+	// https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/elastic_beanstalk_environment#option-settings
+	dynamic "setting" {
+		for_each = var.settings
+		content {
+			name = setting.value["name"]
+			namespace = setting.value["namespace"]
+			value = setting.value["value"]
+		}
+	}
+}
---
+++ v1/aws/lambda/v1/main.tf
@@ -0,0 +1,45 @@
+locals {
+	lambda_zip = "${var.source_directory}/terraform_${var.name}.zip"
+}
+
+data "archive_file" "default" {
+	excludes = concat(["terraform_${var.name}.zip"], var.excluded_files)
+	output_path = local.lambda_zip
+	source_dir = var.source_directory
+	type = "zip"
+}
+data "aws_iam_policy_document" "default" {
+	version = "2012-10-17"
+
+	statement {
+		actions = ["sts:AssumeRole"]
+		effect = "Allow"
+
+		principals {
+			identifiers = ["lambda.amazonaws.com"]
+			type = "Service"
+		}
+	}
+}
+resource "aws_iam_role" "default" {
+	assume_role_policy = data.aws_iam_policy_document.default.json
+}
+resource "aws_iam_role_policy_attachment" "default" {
+	policy_arn  = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+	role = aws_iam_role.default.name
+}
+resource "aws_lambda_function" "default" {
+	filename = local.lambda_zip
+	function_name = var.name
+	handler = var.handler
+	memory_size = ceil(var.memory_mb)
+	role = aws_iam_role.default.arn
+	runtime = var.runtime
+	source_code_hash = data.archive_file.default.output_base64sha256
+	timeout = var.timeout_after_seconds
+
+	environment {
+		// * Environmental keys must not container hyphens "-" https://stackoverflow.com/a/60885479
+		variables = merge([for env in var.environment: { (env["key"]) = (env["value"]) }]...)
+	}
+}
