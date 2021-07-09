5e37de45a324dfb22174a11bffad3869028b8e1d
logak-devops <84037413+logak-devops@users.noreply.github.com>
2021-07-09 11:06:18 +0100

    Update main.tf

--- v0.15/aws-lambda/v1/main.tf
+++ v0.15/aws-lambda/v1/main.tf
@@ -61,9 +61,9 @@ EOF
 
 
 
-resource "aws_iam_role" "default" {
-	assume_role_policy = data.aws_iam_policy_document.default.json
-}
+#resource "aws_iam_role" "default" {
+#	assume_role_policy = data.aws_iam_policy_document.default.json
+#}
 resource "aws_iam_role_policy_attachment" "default" {
 	policy_arn  = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 	role = aws_iam_role.default.name
