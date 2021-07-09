95a7387b955e2291fd75a352a333195df09a4ba5
logak-devops <84037413+logak-devops@users.noreply.github.com>
2021-07-09 11:04:02 +0100

    Update main.tf

--- v0.15/aws-lambda/v2/main.tf
+++ v0.15/aws-lambda/v2/main.tf
@@ -67,7 +67,7 @@ resource "aws_lambda_function" "default" {
 	function_name = var.name
 	handler = var.handler
 	memory_size = ceil(var.memory_mb)
-	role = aws_iam_role.default.arn
+	role = aws_iam_role.default.name
 	runtime = var.runtime
 	source_code_hash = data.archive_file.default.output_base64sha256
 	timeout = var.timeout_after_seconds
